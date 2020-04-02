7126f08c6177facfeee292638414d7b1bd21cf7d
louyinlong <1092755001@qq.com>
2020-04-02 17:00:43 +0800

    vinventory-app
    Signed-off-by: louyinlong <1092755001@qq.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'inventory-app';
+  title: string;
+  imgUrl: string;
+  url: string;
+
+  constructor() {
+    this.title = 'hello';
+    this.imgUrl = '/assets/imgs/ng-book2.png';
+    this.url = 'http://www.baidu.com';
+  }
+
+  onBtnClicked() {
+    console.log('hahahaha!');
+  }
+
+  onInput(evt: Event) {
+    this.title = (<HTMLInputElement>evt.target).value;
+  }
 }
